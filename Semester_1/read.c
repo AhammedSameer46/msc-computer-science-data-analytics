e546864cae54257a491d29eb9145d4dffc06e682
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-07-09 22:39:59 +0530

    Ds lab program

---
+++ Semester_1/read.c
@@ -0,0 +1,12 @@
+include<stdio.h>
+void main()
+{
+	char name[15];
+	int age;
+	printf("Eter your name");
+	scanf("%s",&name);
+	printf("enter your age");
+	scanf("%d",&age);
+	printf("Hello %s, you are %d years old",name,age);
+	
+}
