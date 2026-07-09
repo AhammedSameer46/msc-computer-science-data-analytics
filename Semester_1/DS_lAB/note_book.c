12a646484730dd426d7da37fe482a2f41c9311af
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-07-09 22:44:03 +0530

    Second program

---
+++ Semester_1/DS_lAB/note_book.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{	int price,qty,total;
+	printf("Enter the price of one notebook");
+	scanf("%d",&price);
+	printf("enter the quantity");
+	scanf("%d",&qty);
+	total=price*qty;
+	printf("total price %d",total);
+	
+}
