8fd5a950c8aaca64aac8a99617e3a8189fa4a4f5
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-08-04 22:49:23 +0530

    ADD

---
+++ Semester_1/Data_Structures/Arrays/nx.c
@@ -0,0 +1,16 @@
+
+insert into event_regn values
+('RE01','EV01','S01','01-feb-2024','registered');
+
+insert into event_regn values
+('RE02','EV02','S02','02-jan-2024','attended');
+
+insert into event_regn values
+('RE03','EV03','S04','03-jan-2024','registered');
+
+insert into event_regn values
+('RE04','EV04','S03','04-dec-2024','absent');
+
+insert into event_regn values
+('RE05','EV05','S03','05-mar-2024','attended');
+
