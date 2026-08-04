a7c5f63cdc0916a58ac09cda38e05074dea83487
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-08-04 22:51:50 +0530

    ADD

--- Semester_1/Data_Structures/Arrays/nx.c
+++ Semester_1/Data_Structures/Arrays/nx.c
@@ -14,3 +14,11 @@ insert into event_regn values
 insert into event_regn values
 ('RE05','EV05','S03','05-mar-2024','attended');
 
+
+insert into dept values('D06','Computer Science','Dr. Anil Kumar','Carmel Block',8075791493);
+insert into dept values('D07','Science','Ahammed','A Block',8075791666);
+INSERT INTO dept
+(dept_id, dept_name, hod_name, office_location, contact_number)
+VALUES
+('D08', 'Physics', 'Ahad', 'P Block', '9074084738');
+insert into dept values('&dept_id','&dept_name','&hod_name','&office_location',&contact_number);
