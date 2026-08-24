dab0e0abe50c7931e4963ead04ac20d18d86a8df
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-08-24 17:02:57 +0530

    ADDED

--- Semester_1/Course/day1.c
+++ Semester_1/Course/day1.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    
+    int a[],i;
+    for (i=0;i<5;i++)
+    {
+        printf("Enter the value of a[%d]: ",i);
+        scanf("%d",&a[i]);
+    }
+    printf("The values of the array are: ");
+    for (i=0;i<5;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
 }
