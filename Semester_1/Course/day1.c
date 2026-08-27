43de27f334cb6f7ca8c0874a476b488bfbc8fa62
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-08-27 20:17:20 +0530

    ADDED

--- Semester_1/Course/day1.c
+++ Semester_1/Course/day1.c
@@ -35,3 +35,14 @@ int main()
     printf("%d",sizeof(r1.length));
     return 0;
 }
+
+
+
+
+#include<stdio.h>
+void main(){
+	int a;
+	printf("Enter temp of patient:");
+	scanf("%d",&a);
+	a = 100>a? printf("Normal"):printf("fever");
+	}
